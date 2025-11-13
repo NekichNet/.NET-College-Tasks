e8375a4f00ccd80d6f047db2a9a83ebd2967386d
Top <Top@WIN-3OJHGFTL2DV>
2025-11-13 11:51:40 +0500

    Socket with helper in progress

---
+++ 13_11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _13_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(
+                AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp
+                );
+            socket.Bind(new IPEndPoint(IPAddress.Loopback, 8080));
+            socket.Listen(2);
+            socket.Accept();
+            SocketMessageHelper.SendMessage(socket, "Hi, client!");
+            Console.WriteLine(SocketMessageHelper.ReceiveMessage(socket));
+            socket.Close();
+        }
+    }
+}
---
+++ 13_11_client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _13_11_client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(
+                AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp
+                );
+            socket.Connect(new IPEndPoint(IPAddress.Loopback, 8080));
+            Console.WriteLine(SocketMessageHelper.ReceiveMessage(socket));
+            SocketMessageHelper.SendMessage(socket, "Hi, server!");
+            socket.Close();
+        }
+    }
+}
