9cbecc800f1e2cd30fd718b456512e334b354ad6
NekichNet <nikitanekich2017@gmail.com>
2025-11-27 11:13:40 +0500

    class 2 in progress

--- 06_11/MainWindow.xaml.cs
+++ 06_11/MainWindow.xaml.cs
@@ -24,62 +24,15 @@ namespace _06_11
             InitializeComponent();
         }
 
-        private void NoteBtn_Click(object sender, RoutedEventArgs e)
+        private void Counter(ProgressBar progressBar)
         {
-            new Thread(OpenNotepad).Start();
+            for (int i = 0; i < 100; i++)
+            {
+                Dispatcher.Invoke(() => progressBar.Value = i);
+                Thread.Sleep(100);
+            }
         }
 
-        private void OpenNotepad()
-        {
-            Process NotepadProcess = Process.Start(@"C:\Windows\system32\notepad.exe");
-            Thread.Sleep(3000);
-            NotepadProcess.Kill();
-        }
 
-        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
-        {
-            new Thread(BackgroundThread).Start();
-        }
-
-        private void BackgroundThread()
-        {
-            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
-                (ThreadStart) delegate ()
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        OutputText.Text = (i).ToString();
-                        Thread.Sleep(500);
-                    }
-                }
-            );
-        }
-
-        private void CmdBtn_Click(object sender, RoutedEventArgs e)
-        {
-            new Thread(OpenCmd).Start();
-        }
-
-        private void OpenCmd()
-        {
-            Process CmdProcess = new Process();
-            CmdProcess.StartInfo.FileName = @"C:\Windows\system32\cmd.exe";
-            CmdProcess.StartInfo.UseShellExecute = false;
-            CmdProcess.StartInfo.RedirectStandardInput = true;
-            CmdProcess.StartInfo.RedirectStandardOutput = true;
-            CmdProcess.Start();
-
-            CmdProcess.StandardInput.WriteLine("dir");
-            String output = CmdProcess.StandardOutput.ReadToEnd();
-
-            CmdProcess.WaitForExit(); // you need to close the console window to continue
-
-            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
-                (ThreadStart)delegate ()
-                {
-                    OutputText.Text = output;
-                }
-            );
-        }
     }
 }
